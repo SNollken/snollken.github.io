c36bce5214f4aa524979c8d7f375ae1ed22ee29a
Sofia <soso.novaes@gmail.com>
2025-06-24 21:05:00 -0300

    ajustes seção ferramentas

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 //espera o conteúdo da página carregar completamente antes de rodar o script
 document.addEventListener('DOMContentLoaded', function() {
-
+    
+    // logica para o botao de start rolar a pagina
     const startButton = document.getElementById('startButton');
-
     const sobreMimSection = document.getElementById('sobre-mim');
 
-    //verificar se ambos os elementos foram encontrados
-    //isso é uma boa prática para evitar erros caso os IDs mudem ou não existam.
     if (startButton && sobreMimSection) {
-
         startButton.addEventListener('click', function() {
-            //quando o botão for clicado, pedir para a seção "sobre-mim"
-            //rolar suavemente para a vista.
             sobreMimSection.scrollIntoView({ behavior: 'smooth' });
         });
-
-    } else {
-        console.warn("Botão 'Start' ou seção 'Sobre Mim' não encontrados. Verifique os IDs no HTML.");
     }
 
 });
